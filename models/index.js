696fb58b8def2568cbd96dae001d6ce4597711db
Alex Mulgrew <alexpmulgrew@gmail.com>
2022-01-19 11:23:00 +0000

    routes added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,23 @@
-import query from "../db/index.js"
+import query from "../db/index.js";
 
 export async function getAllDays() {
-    await query
+  const data = await query("SELECT * FROM bc_tracker");
+  return data.rows;
+}
+
+export async function getDayById(id) {
+  const data = await query(`SELECT [id] FROM bc_tracker`);
+  return data.rows;
+}
+
+export async function updateTopicsByID(id, updates) {
+  //take in the id, take in an updated user
+  //find the user with the id matching what we were given.
+  //replace that user with the updates
+  //return new user
+  const foundIndex = topics.findIndex(function (topics) {
+    return topics.id === id;
+  });
+  topics[foundIndex] = updates;
+  return topics[foundIndex];
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 import express from "express";
 const router = express.Router();
+import { getAllDays, getDayById } from "../models/index.js";
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
+router.get("/", async function (req, res, next) {
+  const data = await getAllDays();
+  res.json({
+    message: "I wish we had some information to give you ☹️",
+    payload: data,
+  });
 });
+
+router.get("/:id", async function(req, res, next){
+  const data = await getDayById(req.params.id);
+  res.json({
+    message: "This is all the data for" + req.params.id,
+    payload: data,
+  });
+});
+
+
+
+
+
 
 export default router;

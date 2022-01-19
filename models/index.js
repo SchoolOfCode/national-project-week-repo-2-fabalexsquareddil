9b9d1f7ecf67459e50db6e3ecbd6f4cf80925ce7
Alexander Telecan <alexander@Alexanders-MacBook-Pro.local>
2022-01-19 16:13:29 +0000

    testing for deployment

--- models/index.js
+++ models/index.js
@@ -1,28 +1,33 @@
-import query from "../db/index.js";
+import query from '../db/index.js';
 
 //For GetAll request
 export async function getAllDays() {
-  const data = await query("SELECT * FROM bc_tracker");
-  return data.rows;
+	const data = await query('SELECT * FROM bc_tracker');
+	return data.rows;
 }
 
 //For GetById request
 export async function getDay(week_day) {
-  const data = await query(`SELECT * FROM bc_tracker WHERE week_day = $1;`, [week_day]);
-  return data.rows;
+	const data = await query(`SELECT * FROM bc_tracker WHERE week_day = $1;`, [
+		week_day,
+	]);
+	return data.rows;
 }
 
 // For PATCH request
-export async function updateTopicsByID(id, updates) {
-  const foundIndex = await query(
-  Update tableName SET topic = '{id:1, week: 1, day:1, topic: "hello", completed: false}' WHERE week_day = req.params.week_day)
-  topics.findIndex(function (topics) {
-    return topics.id === id;
-  });
-  topics[foundIndex] = updates;
-  return topics[foundIndex];
-}
+export async function updateTopicsByWeekDay(weekDay, updates) {
+	console.log('called updateWeekDay');
+	const data = await query(
+		`UPDATE bc_tracker SET topics=$1 WHERE week_day = '${weekDay}' RETURNING *;`,
+		[updates]
+	);
 
+	// topics.findIndex(function (topics) {
+	//   return topics.id === id;
+	// });
+	// topics[foundIndex] = updates;
+	// return topics[foundIndex];
+}
 
 // export async function updateUserByID(id, updatedUser) {
 //   let userIndex = users.findIndex(function isUseridFound(user){
